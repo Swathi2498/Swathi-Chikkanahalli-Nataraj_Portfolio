1ab8d0fc14aeead1c8f0542fa3c87d0f7451159e
swathi2498 <swathinataraj8926@gmail.com>
2024-12-16 16:41:59 +0100

    completing Exercise 2.3

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,1 +1,24 @@
 console.log("Hello! Welcome to my Portfolio Site")
+
+function menuToggle() {
+}
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+  }
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+  
+  $('#click_advance').click(function() {
+    $('#display_advance').toggle('1000');
+    $("i", this).toggleClass("fas fa-bars fas fa-x");
+  });
